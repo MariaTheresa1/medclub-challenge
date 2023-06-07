0b21bc580ce9e7432789de6391fe40ac4ee846ae
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2023-06-06 23:03:50 -0300

    feat: adição da configuração de paginação

--- medclub_challenge/urls.py
+++ medclub_challenge/urls.py
@@ -10,3 +10,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
