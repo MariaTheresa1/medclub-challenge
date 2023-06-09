2e9560b202da6cfc00c410cfdb1d6ad749513813
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2023-06-09 20:40:14 -0300

    docs: adicionado comentários explicativos sobre as urls

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,13 @@ from users.views import UserCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
 
+    # Retorna o token de um usuário 
     path('api-token-auth/', obtain_auth_token),
 
+    # Permite a criação de um usuário (POST) sem a necessidade de altenticação
     path('', UserCreateView.as_view(), name='user-create'),
 
+    # Permite o GET, PUTH, PATCH e DELETE de um usuário autenticado
     path(
         '<int:pk>/', 
         UserRetrieveUpdateDestroyView.as_view(), 
