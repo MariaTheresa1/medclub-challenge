866bcac1a9df4f183c81004b2d4f4b9be8e6dd03
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2023-06-09 20:42:31 -0300

    refactor: conserto do problema de autentição ao tentar gerar o token do usuário

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
@@ -9,15 +9,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 
-                  'last_name', 'email', 'password', 'groups']
+                  'last_name', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
         read_only_fields = ["id", "date_joined"]
-
-# Grupos
-
-
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['id', 'name']
-
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(password=password, **validated_data)
+        return user
